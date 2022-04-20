4446409b9feab7d841a0d56ead70253a6f11ce2c
Sam Garcia <samuel.gsuero@gmail.com>
2022-04-20 11:49:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { mockData } from './mock-data';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <CitySearch />
-        <EventList />
+        <EventList events={mockData} />
         <NumberOfEvents />
       </div>
     );
