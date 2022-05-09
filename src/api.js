b414d5ef54b86430bfcf0bf12cc1354c024c81b6
Sam Garcia <samuel.gsuero@gmail.com>
2022-05-09 12:14:54 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -64,7 +64,13 @@ export const getEvents = async () => {
 
     if (window.location.href.startsWith('http://localhost')) {
         NProgress.done();
-        return mockData;
+        return { events: mockData, locations: extractLocations(mockData) };
+    }
+
+    if (!navigator.onLine) {
+        const { events } = await localStorage.getItem("lastEvents");
+        NProgress.done();
+        return { events: JSON.parse(events), locations: extractLocations(events) };
     }
 
     const token = await getAccessToken();
@@ -75,11 +81,11 @@ export const getEvents = async () => {
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
-            localStorage.setItem('lastEvents', JSON.stringify(result.data));
+            localStorage.setItem('lastEvents', JSON.stringify(result.data.events));
             localStorage.setItem('locations', JSON.stringify(locations));
         }
         NProgress.done();
-        return result.data.events;
+        return { events: result.data.events, locations };
     }
 
 };
