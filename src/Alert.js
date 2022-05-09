e01a2e45b8497d2f23a474aedf97336a52cf826b
Sam Garcia <samuel.gsuero@gmail.com>
2022-05-09 10:12:57 +0200

    Update Alert.js

--- src/Alert.js
+++ src/Alert.js
@@ -35,5 +35,12 @@ class ErrorAlert extends Alert {
     }
 }
 
-export { InfoAlert };
-export { ErrorAlert };
+class OfflineAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'red';
+        this.fontSize = '12px';
+    }
+}
+
+export { InfoAlert, ErrorAlert, OfflineAlert };
